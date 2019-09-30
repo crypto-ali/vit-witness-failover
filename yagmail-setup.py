362a3a4c6a7ca7f96720fd99a4aa8caf9d213d80
Ali Monhi <monhi@protonmail.com>
2019-09-30 09:54:43 -0400

    Yagmail modifications

--- yagmail-setup.py
+++ yagmail-setup.py
@@ -6,12 +6,16 @@ load_dotenv()
 
 # Variables
 FROM = os.getenv('FROM_ADDRESS')
+FROM_PASS = os.getenv('FROM_PASS')
 TO = os.getenv('TO_ADDRESS')
 
-yag = yagmail.SMTP(FROM, oauth2_file='~/oauth2_creds.json')
+#yag = yagmail.SMTP(FROM, oauth2_file='~/oauth2_creds.json')
+yag = yagmail.SMTP(FROM, FROM_PASS)
 
 to = TO
 subject = 'Can you read me?'
 body = "If so, then Yagmail is working with oauth2."
+body2 = "If so, then Yagmail is working with email & app password."
 
-yag.send(to, subject, body)
+#yag.send(to, subject, body)
+yag.send(to, subject, body2)
